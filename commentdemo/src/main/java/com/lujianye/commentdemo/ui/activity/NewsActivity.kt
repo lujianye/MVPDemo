48cf02fbbc8648e6fd567a52fecde3d28645b821
lujianye <1260468819@qq.com>
2018-03-09 17:37:00 +0800

    完善评论功能

--- commentdemo/src/main/java/com/lujianye/commentdemo/ui/activity/NewsActivity.kt
+++ commentdemo/src/main/java/com/lujianye/commentdemo/ui/activity/NewsActivity.kt
@@ -1,17 +1,14 @@
 package com.lujianye.commentdemo.ui.activity
 
-import android.view.WindowManager
 import android.widget.FrameLayout
-import com.lujianye.commentdemo.base.BaseCommentActivity
-import com.lujianye.commentdemo.base.BaseCommentActivity1
 import com.lujianye.commentdemo.base.BaseCommentActivity2
 
 /**
  * Description : TODO
  * Author : lujianye
- * Date : 2018/3/8
+ * Date : 2018/3/9
  */
-class VideoActivity : BaseCommentActivity2() {
+class NewsActivity : BaseCommentActivity2() {
     override fun initUp(fl_content_up: FrameLayout) {
 
     }
